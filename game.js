cdf47c497c73f82afa37ad5b1b537e7f9e0f6c3f
Fishi736 <53339462+Fishi736@users.noreply.github.com>
2020-11-01 12:32:03 +0530

    done

--- game.js
+++ game.js
@@ -12,9 +12,9 @@ $submitButton.onclick = () => {
     lives--; //decrementing the lives
 
     if (userInput == guessNumber) {
-        location.replace("https://fishi736.github.io/win.html");
+        location.href="https://fishi736.github.io/win.html";
     } else if (lives == 0) {
-        location.replace("https://fishi736.github.io/lose.html") ;
+        location.href="https://fishi736.github.io/lose.html";
     } else if (userInput > guessNumber) {
         message = `oops! Your guess is too high. You have ${lives} remaining`;
     } else if (userInput < guessNumber) {
